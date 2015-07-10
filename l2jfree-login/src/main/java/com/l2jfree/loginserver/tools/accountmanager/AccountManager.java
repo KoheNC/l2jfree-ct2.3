da7ece6ce7d1bb56022334b70388b729708cdda6
lord_rex <lordrex34@hotmail.com>
2015-07-10 14:07:33 +0200

    ACM / GS Register

--- l2jfree-login/src/main/java/com/l2jfree/loginserver/tools/accountmanager/AccountManager.java
+++ l2jfree-login/src/main/java/com/l2jfree/loginserver/tools/accountmanager/AccountManager.java
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.l2jfree.accountmanager;
+package com.l2jfree.loginserver.tools.accountmanager;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- l2jfree-login/src/main/java/com/l2jfree/loginserver/tools/gsregistering/GameServerRegister.java
+++ l2jfree-login/src/main/java/com/l2jfree/loginserver/tools/gsregistering/GameServerRegister.java
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.l2jfree.gsregistering;
+package com.l2jfree.loginserver.tools.gsregistering;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
