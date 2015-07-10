37e265699218a6a1893e393de0719f7f7bafb540
lord_rex <lordrex34@hotmail.com>
2015-07-10 18:42:05 +0200

    IRC refactor

--- l2jfree-core/src/main/java/com/l2jfree/gameserver/handler/irccommands/IrcHelp.java
+++ l2jfree-core/src/main/java/com/l2jfree/gameserver/handler/irccommands/IrcHelp.java
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.l2jfree.gameserver.handler.irccommandhandlers;
+package com.l2jfree.gameserver.handler.irccommands;
 
 import com.l2jfree.gameserver.handler.IIrcCommandHandler;
 import com.l2jfree.gameserver.instancemanager.IrcManager;
